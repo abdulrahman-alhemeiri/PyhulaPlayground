e8751468d95028ca9081f9f865efb1eb73e252a9
Abdulrahman Alhemeiri <alhadhrami93@gmail.com>
2026-01-09 14:14:34 +0400

    Optimize path in Challenge2

--- Challenge2.py
+++ Challenge2.py
@@ -17,11 +17,11 @@ if is_discovery_phase:
     Utils.save_maze_to_file(maze, fileName)
     drone.land()
 else:
-    object_coordinates = [(2, 0), (3, 1), (2, 1)]
+    object_coordinates = [(1, 0), (2, 0), (3, 0)]
     object_directions = {
+        (1, 0): "South",
         (2, 0): "West",
-        (2, 1): "East",
-        (3, 1): "West"
+        (3, 0): "South"
     }
     paths = PathFinder.astar_multi_goal_straight_preference(maze, start, object_coordinates)
     optimized_paths = []
@@ -31,8 +31,8 @@ else:
 
     is_video_mode = True
     drone.take_off(is_video_mode)
-    for i in range(len(paths)):
-        drone.traverse_path(paths[i])
+    for i in range(len(optimized_paths)):
+        drone.traverse_path(optimized_paths[i])
         current_block = drone.get_current_block()
         print(f"(main): Performing object detection at block {tuple(current_block)} which has direction {object_directions[tuple(current_block)]}")
         drone.perform_detection(object_directions[tuple(current_block)])
